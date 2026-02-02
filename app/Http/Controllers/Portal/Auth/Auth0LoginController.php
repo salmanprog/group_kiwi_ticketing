427402dfe755f5c0341651ca16d4c124c507001d
salmanprog <m.salmanprog@gmail.com>
2026-02-02 18:04:49 +0500

    update estimate

--- app/Http/Controllers/Portal/Auth/Auth0LoginController.php
+++ app/Http/Controllers/Portal/Auth/Auth0LoginController.php
@@ -311,7 +311,19 @@ class Auth0LoginController extends Controller
     }
 
 
+    public function redirectToAuth0()
+    {
+    $auth0 = new \Auth0\SDK\Auth0([
+        'domain' => env('AUTH0_DOMAIN'),
+        'clientId' => env('AUTH0_CLIENT_ID'),
+        'clientSecret' => env('AUTH0_CLIENT_SECRET'),
+        'redirectUri' => env('AUTH0_REDIRECT_URI'),
+        'cookieSecret' => substr(base64_decode(str_replace('base64:', '', env('APP_KEY'))), 0, 32),
+        'strategy' => 'webapp',
+    ]);
 
+    return redirect($auth0->login());
+    }
     protected function redirectByRole($user)
     {
         $getCompany = CompanyUser::getCompany($user->id);
